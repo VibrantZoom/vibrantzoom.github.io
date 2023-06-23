3075ee7714e1f9fddcad080eb3e839641378f580
Mystery135 <websiteemail12345678910@gmail.com>
2023-06-23 03:05:45 -0700

    added game wow

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let viewcount = 0;
+
+var documentName = document.title;
+if(localStorage.getItem("localStorageEnabled")){
+  if(documentName != "General Knowledge Quiz"){
+    document.getElementById("disclaimerBanner").style.display = "none";
+    document.getElementById("nav").style.borderRadius = '10px';  
+  }
+  
+
+}
+
 let localStorageEnabled = true;
 function enableCookies(){
   document.getElementById("disclaimerBanner").style.display = "none";
@@ -17,7 +27,6 @@ function disableCookies(){
 
   localStorageEnabled = false;
 }
-
 
 // addEventListener("mousemove", (event) => {
 //   const rect = event.target.getBoundingClientRect();
@@ -141,7 +150,7 @@ var variableUpdates = {
   '--background-color': '#ff5e5e',
   '--primary-button-color': '#96bba7',
   '--secondary-button-color': '#88cca8',
-  '--accent-color': '#745d8e',
+  '--accent-color': '#4fb580',
   '--subtitle-color': 'darkgrey',
   '--nav-color': 'lightcoral',
   '--link-active': '#02f9c0',
@@ -186,12 +195,244 @@ if(localStorageEnabled){
   }
   
 }
+const questions = [
+  'What is the largest planet in our solar system?',
+  'What is this?',
+  'What is the capital city of Japan?',
+  'What is the largest mammal in the world?',
+  'What is the smallest country in the world by land area?',
+  'What is the largest ocean in the world?',
+  'What is the longest river in the world?',
+  'What is the smallest planet in our solar system?',
+  'What is the capital of China?',
+  'What is the lowest point on Earth?',
+  'What is the shortest river in the world?',
+  'How many continents are there?',
+  'What is the highest temperature ever recorded?',
+  'What is the tallest animal on Earth?',
+  'What is the smallest mammal on Earth by mass?',
+  'What is the most populated country on Earth?',
+  'What is the highest mountain on Earth?',
+  'What is the chemical symbol for gold?',
+  'What is the largest desert on Earth?',
+  'What is the largest country on Earth by land area?',
+  'What is the largest continent on Earth by land area?'
+];
 
 
-function onLoadIndex(){
 
+const images = [
+  'https://images.unsplash.com/photo-1614642264762-d0a3b8bf3700?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80',
+  'images/mysterycountry.png',
+  'https://images.unsplash.com/photo-1542051841857-5f90071e7989?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1535940360221-641a69c43bac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1166&q=80',
+  'https://images.unsplash.com/photo-1634324173063-909962333bca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80',
+  'https://images.unsplash.com/photo-1615529328331-f8917597711f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1555412654-72a95a495858?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+
+];
+const option1 = [
+  'Saturn',
+  'Greenland',
+  'Canada',
+  'Elephant',
+  'Vatican City',
+  'Pacific Ocean',
+  'Wadi',
+  'Mercury',
+  'Beijing',
+  'Dead Sea',
+  'Roe River',
+  'Seven',
+  '56.7 °C (134.1 °F)',
+  'Elephant',
+  'Etruscan shrew',
+  'India',
+  'K2',
+  'Ag',
+  'Sahara Desert',
+  'China',
+  'Africa'
+];
+
+const option2 = [
+  'Jupiter',
+  'Tokyo',
+  'Tokushima',
+  'Ender Dragon',
+  'Maza',
+  'Atlantic Ocean',
+  'Nile',
+  'Mars',
+  'Shanghai',
+  'Mariana Trench',
+  'Amazon River',
+  'Five',
+  '58.8 °C (137.8 °F)',
+  'Giraffe',
+  'Mouse',
+  'China',
+  'Mount Everest',
+  'Au',
+  'Antarctic Desert',
+  'Russia',
+  'Asia'
+];
+
+const option3 = [
+  'Earth',
+  'Canada',
+  'Beijing',
+  'Hippopotamus',
+  'London',
+  'Indian Ocean',
+  'Boteti',
+  'Venus',
+  'Guangzhou',
+  'Mount Everest',
+  'Yangtze River',
+  'Six',
+  '57.8 °C (136.0 °F)',
+  'Blue whale',
+  'Bat',
+  'United States',
+  'Mount Kilimanjaro',
+  'Fe',
+  'Gobi Desert',
+  'Canada',
+  'North America'
+];
+const option4 = [
+  'ROXs 42 Bb',
+  'Metaphor',
+  'Tokyo',
+  'Blue whale',
+  'Saint-Louis-de-Gonzague-du-Cap-Tourmente',
+  'Southern Ocean',
+  'Ruzizi',
+  'Jupiter',
+  'Hong Kong',
+  'Challenger Deep',
+  'Nile River',
+  'Eight',
+  '55.0 °C (131.0 °F)',
+  'Ostrich',
+  'Hedgehog',
+  'Indonesia',
+  'Mount Fuji',
+  'Hg',
+  'Arabian Desert',
+  'United States',
+  'Europe'
+];
+const answers = [
+  'Jupiter',
+'Canada',
+  'Tokyo',
+  'Blue whale',
+  'Vatican City',
+  'Pacific Ocean',
+  'Nile',
+  'Mercury',
+  'Beijing',
+  'Challenger Deep',
+  'Roe River',
+  'Seven',
+  '56.7 °C (134.1 °F)',
+  'Giraffe',
+  'Etruscan shrew',
+  'China',
+  'Mount Everest',
+  'Au',
+  'Antarctic Desert',
+  'Russia',
+  'Asia'
+];
+let wrong = 0;
+let right = 0;
+let qnum = 0;
+let qremainingHtml = document.getElementById("qremaining");
+let questionHtml = document.getElementById("triviaquestion"); 
+let imageHtml = document.getElementById("qimage");
+let option1btn = document.getElementById("option1");
+let option2btn = document.getElementById("option2");
+let option3btn = document.getElementById("option3");
+let option4btn = document.getElementById("option4");
+let answersHtml = document.getElementById("answekey");
+let multichoicebtns = document.getElementById("multichoicebtns");
+let goToNextQuestionBtn = document.getElementById("go-to-next-question");
+const optionbtns = document.querySelectorAll(".optionbtn");
+const homebtn = document.getElementById("gohome");
+const playagainbtn = document.getElementById("playagain");
+let qhaventdone = [];
+let currentq = 0;
+let amntofqs = 10;
+for (let i = 1; i<=answers.length; i++) {
+qhaventdone.push(i);
 }
 
+function startTrivia(){
+  qremainingHtml.innerText = "Questions Remaining: " + (amntofqs - qnum);
+if(qnum >= amntofqs){
+  answersHtml.innerText = "gud job. try again for a better score. questions are randomly picked from a pool of 21.";
+  answersHtml.style.color = "white";
+  questionHtml.innerText = "You got " + (wrong) + " questions wrong and "+ (right) +" questions right! Score: " + ((right/questions.length)*100) + "%";
+imageHtml.src = "https://images.unsplash.com/photo-1580893211123-627e0262be3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80";
+homebtn.style.display = "block";
+playagainbtn.style.display = "block";
+goToNextQuestionBtn.style.display = "none";
+return;
+}
+  answersHtml.innerText = "";
+  optionbtns.forEach(btn => {
+    btn.style.display = "block";
+  });
+  goToNextQuestionBtn.style.display = "none";
+  let toremove = Math.floor(Math.random() * qhaventdone.length);
+  currentq = qhaventdone[toremove];
+  currentq--;
+  qhaventdone.splice(toremove, 1);
+
+
+  questionHtml.innerText = questions[currentq];
+  var options = [option1[currentq], option2[currentq], option3[currentq], option4[currentq]];
+
+  // Shuffle the options array
+  for (var i = options.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = options[i];
+    options[i] = options[j];
+    options[j] = temp;
+  }
+  
+  option1btn.innerText = options[0];
+  option2btn.innerText = options[1];
+  option3btn.innerText = options[2];
+  option4btn.innerText = options[3];
+  
+  imageHtml.src = images[currentq];
+}
+function nextQuestion(btn) {
+if(btn.innerText == answers[currentq]){
+answersHtml.innerText = "Correct! Correct answer: " + (answers[currentq]);
+answersHtml.style.color = "lightgreen";
+right++;
+}else{
+  answersHtml.innerText = "Incorrect! Correct answer: " + (answers[currentq]);
+  answersHtml.style.color = "red";
+  wrong++;
+}
+
+optionbtns.forEach(btn => {
+  btn.style.display = "none";
+});
+goToNextQuestionBtn.style.display = "block";
+
+}
+function goToNextQuestion(){
+  qnum++;
+startTrivia();
+}
 
 // function onLoadIndex(){
 //   console.log("dsfsadf");
@@ -408,6 +649,13 @@ async function phosphoricacidclick(){
 
 function learnmore(){  
   let rand = getRandomInt(3);
+  if(localStorageEnabled){
+    if(!localStorage.getItem("rickrld")){
+      window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+      localStorage.setItem("rickrld", true);
+      return;
+    }
+  }
   if(rand == 0){
     window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
   }else if (rand == 1){
@@ -445,7 +693,6 @@ function getRandomInt(max) {
 
 
 function scrollToElement(elementId) {
-  console.log(elementId);
   var element = document.getElementById(elementId);
   if (element) {
     var elementRect = element.getBoundingClientRect();
